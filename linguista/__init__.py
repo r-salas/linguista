44d39d6b89cc1932cdf47666b772594c9f5eaa8b
Rubén Salas <ruben.sls@outlook.com>
2024-05-19 22:36:19 +0200

    Add structure for library

---
+++ linguista/__init__.py
@@ -0,0 +1,11 @@
+#
+#
+#   Linguista
+#
+#
+
+__version__ = '0.1.0'
+
+__author__ = 'Rubén Salas'
+
+__description__ = 'Build conversational assistants easily and with the power of LLMs'
---
+++ setup.py
@@ -0,0 +1,35 @@
+import re
+import os
+
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), "linguista", '__init__.py')) as f:
+    init_py = f.read()
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), "r") as f:
+    long_description = f.read()
+
+
+def get_property(prop):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), init_py)
+    return result.group(1)
+
+
+setup(
+    name='linguista',
+    version=get_property("__version__"),
+    author=get_property('__author__'),
+    author_email="me@rubensalas.ai",
+    description=get_property("__description__"),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/r-salas/linguista',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
