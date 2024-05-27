f953c60b6cf8bbae81fc49a1b5a17d642b03286d
Rubén Salas <ruben.sls@outlook.com>
2024-05-27 16:28:46 +0200

    Add example for api

--- api_booking.py
+++ api_booking.py
@@ -10,10 +10,52 @@ import linguista
 
 from typing import Optional
 
-def confirmation_prompt_for_create_booking(bot: linguista.Bot, )
 
-@linguista.flow("create_booking", "Create a new booking")
-def create_booking(bot: linguista.Bot, num_people: int, date_time: datetime.datetime, name: str, phone_number: str,
-                   observations: Optional[str] = None,
-                   confirmation: Annotated[bool, linguista.Slot(prompt="Do you confirm the booking?")]):
-    ...
+class TransferMoneyFlow(linguista.Flow):
+
+    amount = linguista.FlowSlot(
+        description="Amount of money to transfer",
+        type=int,
+        required=True
+    )
+    recipient = linguista.FlowSlot(
+        description="Recipient name",
+        type=linguista.types.Categorical(["Alice", "Bob", "Charlie"])
+    )
+
+    transfer_confirmation = linguista.FlowSlot(
+        description="Confirm the transfer",
+        type=bool
+    )
+
+    def start(self, bot: linguista.Bot):
+        bot.reply("Welcome to the money transfer service!")
+        bot.reply("Please follow the instructions to complete the transfer.")
+
+        return linguista.chain(
+            bot.actions.ask(self.amount, prompt="How much money would you like to transfer?", id="ask-amount"),
+            self.validate_amount
+        )
+
+    @linguista.action
+    def validate_amount(self, bot: linguista.Bot, amount: int):
+        if amount < 0:
+            bot.reply("Amount must be positive")
+            return linguista.actions.Next("ask-amount")
+
+        return self.ask_recipient
+
+    @linguista.action
+    def ask_recipient(self, bot: linguista.Bot, amount: int):
+        return bot.actions.ask(self.recipient, prompt=f"Who would you like to transfer {amount}€ to?") >> self.ask_confirmation
+
+    @linguista.action
+    def ask_confirmation(self, bot: linguista.Bot, amount: int, recipient: str):
+        return linguista.actions.Ask(self.transfer_confirmation, prompt=f"Are you sure you want to transfer {amount}€ to {recipient}?") >> self.validate_confirmation
+
+    @linguista.action
+    def validate_confirmation(self, bot: linguista.Bot, transfer_confirmation: bool, amount: int, recipient: str):
+        if transfer_confirmation:
+            bot.reply("Transfer of ${} to {} completed".format(amount, recipient))
+        else:
+            bot.reply("Ok, I won't transfer the money")
